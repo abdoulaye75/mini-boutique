5476389e5b48cb859590562b2d65b45bbfb99ec5
abdoulaye <abdoulayedabo@laposte.net>
2017-04-11 20:35:39 +0200

    page d'accueil

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Randonneurs </title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<h1> S'inscrire ou se connecter </h1>
+	<a href="signup.php"> S'inscrire </a>
+	<a href="login.php"> Se connecter </a>
+
+</body>
+</html>
